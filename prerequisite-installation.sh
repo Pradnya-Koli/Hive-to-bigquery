7e1e05cc2db1d2fbc1513f57412c931e3e904a3c
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-03-10 00:06:13 +0530

    Update prerequisite-installation.sh

--- prerequisite-installation.sh
+++ prerequisite-installation.sh
@@ -16,3 +16,10 @@ pip install google-cloud-storage
 sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev
 pip install sasl
 pip install thrift_sasl
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt-get update
+mkdir projects
+cd projects 
+sudo apt-get install sbt
