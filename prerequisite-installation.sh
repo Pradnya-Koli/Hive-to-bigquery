68b2959bbcf773f4153dc64514282ec689c5b49f
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-03-09 23:59:06 +0530

    Create prerequisite-installation.sh

---
+++ prerequisite-installation.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+pip3 install pandas
+pip3 install findspark
+pip3 install pyhive
+pip install thrift
+pip install pyhive
+pip install findspark
+ pip install google
+pip install google-api-python-client
+pip install google-cloud-bigquery
+pip install google-cloud-storage
+sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev
+pip install sasl
+pip install thrift_sasl
