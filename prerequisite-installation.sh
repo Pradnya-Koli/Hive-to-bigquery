2a0e89c25cc002e66e6becedfe13f9a14440444c
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-03-10 00:02:27 +0530

    Update prerequisite-installation.sh

--- prerequisite-installation.sh
+++ prerequisite-installation.sh
@@ -1,5 +1,8 @@
 sudo apt-get update
 sudo apt-get install python3-pip
+wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+sh /tmp/Anaconda3-2019.10-Linux-x86_64.sh
+source ~/.bashrc
 pip3 install pandas
 pip3 install findspark
 pip3 install pyhive
