c32012bb02a48d51b12aea20bee80c516538c489
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-03-09 21:20:48 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,90 @@
+# script1.sh
+#!/bin/bash
+
+BUCKET_NAME="demo-gcs-bq"
+
+# Check if the bucket exists
+if gsutil ls "gs://${BUCKET_NAME}" >/dev/null 2>&1; then
+  echo "Bucket already exists: ${BUCKET_NAME}"
+else
+  echo "Bucket does not exist: ${BUCKET_NAME}"
+  # Create the bucket
+  gsutil mb "gs://${BUCKET_NAME}"
+  echo "Bucket created: ${BUCKET_NAME}"
+fi
+
+PROJECT_ID="searce-practice-data-analytics"
+DATASET_NAME="hive_to_bigquery"
+
+# Check if the dataset exists
+if bq show "${PROJECT_ID}:${DATASET_NAME}" >/dev/null 2>&1; then
+  echo "Dataset already exists: ${DATASET_NAME}"
+else
+  echo "Dataset does not exist: ${DATASET_NAME}"
+  # Create the dataset
+  bq mk --project_id "${PROJECT_ID}" "${DATASET_NAME}"
+  echo "Dataset created: ${DATASET_NAME}"
+fi
+
+# Set another variable
+export from_path="hdfs://accelerator-hive-bq-m/user/hive/warehouse/hivedemo.db/"
+echo "from_path ==>",$from_path
+export to_path="gs://demo-gcs-bq"
+echo "to_path ==>",$to_path
+export project="searce-practice-data-analytics"
+echo "-------> project-Id <--------",$project
+export bucket_name="demo-gcs-bq"
+echo "------->bucket_name <--------",$bucket_name
+export table_id="searce-practice-data-analytics.hive_to_bigquery."
+echo "-------->table_id <---------------",$table_id
+export dataset_id="searce-practice-data-analytics.hive_to_bigquery."
+echo "------->dataset_id<--------------",$dataset_id
+export table_idl="searce-practice-data-analytics.hive_to_bigquery."
+
+# Prompt the user to select a script
+echo "Please select a script to execute:"
+echo "1. Historic-load"
+echo "2. Incremental-load"
+echo "3. Schema evolution"
+read -p "Enter the script number: " script_number
+
+# Execute the selected script
+
+case "$script_number" in
+  "1")
+    echo "Executing Historic Migration script..........."
+    python3 multithread.py
+    ;;
+  "2")
+    echo "Executing Incremental Migration script........"
+    cd /opt/projects
+    sbt run
+    ;;
+  "3")
+    echo "Executing Schema Evolution Migration script......"
+    echo "Please select a schema evolution:"
+    echo "1. Add_columns"  
+    echo "2. Rename columns" 
+    echo "3. Drop columns"
+    read -p "select the change of schema: " number
+    case "$number" in 
+    "1")
+      echo "executing the add column function..........."
+      python3 main.py --source ADD_COLUMNS
+      ;;
+    "2")
+      echo "executing the rename column function..........."  
+      python3 main.py --source RENAME_COLUMN
+      ;;
+    "3")
+      echo "executing the drop column function........"
+      python3 main.py --source DROP_COLUMN
+      ;;
+    *)
+    echo "Invalid number entered."
+    ;;
+    esac
+#  *)
+  echo "Invalid number entered."
+  ;;
+esac
