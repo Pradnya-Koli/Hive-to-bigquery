a5dc12a2db5b10f610920f77e8a2834df8301b19
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-03-10 00:41:49 +0530

    Update prerequisite-installation.sh

--- prerequisite-installation.sh
+++ prerequisite-installation.sh
@@ -23,5 +23,5 @@ echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/a
 echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
 curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
 sudo apt-get update
-cd incremental
+cd Incremental
 sudo apt-get install sbt
