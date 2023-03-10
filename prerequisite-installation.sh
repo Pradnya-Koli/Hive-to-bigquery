8909757ea62854401711d42917e9d94145ecece6
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-03-10 12:53:10 +0530

    Update prerequisite-installation.sh

--- prerequisite-installation.sh
+++ prerequisite-installation.sh
@@ -1,4 +1,20 @@
 
+sudo su
+cd /opt
+sudo apt-get update
+sudo apt-get install python3-pip
+wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+sh /tmp/Anaconda3-2019.10-Linux-x86_64.sh
+source ~/.bashrc
+sleep 3s
+echo "   activate conda "
+conda activate
+sleep 3s
+echo "   cloning the git repo "
+git clone https://github.com/Pradnya-Koli/Hive-to-bigquery.git
+
+echo " Installing all the required packages :
+
 pip3 install pandas
 pip3 install findspark
 pip3 install pyhive
@@ -12,6 +28,17 @@ pip install google-cloud-storage
 sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev
 pip install sasl
 pip install thrift_sasl
+echo "  setup the Hive acid transaction configuration file "
+bash auto-conf-setup-incremental.sh
+
+echo "  Restart the hive server "
+sudo systemctl restart hive-server2
+
+echo "successfully completed the setup for hive to bigquery migration run the next file start.sh "
+
+cd Hive-to-bigquery
+createinsert.sql
+
 wget https://repository.cloudera.com/artifactory/libs-release-local/com/hortonworks/hive/hive-warehouse-connector-spark3_2.12/1.0.0.1.18.7215.0-43/hive-warehouse-connector-spark3_2.12-1.0.0.1.18.7215.0-43.jar
 wget https://storage.googleapis.com/spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.29.0.jar
 echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
