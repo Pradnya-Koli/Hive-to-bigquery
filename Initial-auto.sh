608e3824364ec259f4f4fbd467198885847e2e76
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-03-10 10:11:38 +0530

    Update Initial-auto.sh

--- Initial-auto.sh
+++ Initial-auto.sh
@@ -1,3 +1,4 @@
+echo " Creating the Dataproc Clusters"
 gcloud dataproc clusters create demo-hive-to-b2 \
 --enable-component-gateway \
 --region us-central1  \
@@ -10,7 +11,7 @@ gcloud dataproc clusters create demo-hive-to-b2 \
 --image-version 2.0-debian10 \
 --project searce-practice-data-analytics \
 --service-account=test-demo@searce-practice-data-analytics.iam.gserviceaccount.com
-
+echo "Connecting To compute ssh "
 gcloud compute ssh demo-hive-to-b2-m --project=searce-practice-data-analytics --zone=us-central1-f
 sudo su
 cd /opt
@@ -20,8 +21,21 @@ sudo apt-get install python3-pip
 wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 sh /tmp/Anaconda3-2019.10-Linux-x86_64.sh
 source ~/.bashrc
-
+echo "   activate conda "
+conda activate
+echo "   cloning the git repo "
 git clone https://github.com/Pradnya-Koli/Hive-to-bigquery.git
+
+echo " Installing all the required packages :
 bash prerequisite-installation.sh
+
+echo "  setup the Hive acid transaction configuration file "
 bash auto-conf-setup-incremental.sh
+
+echo "  Restart the hive server "
+sudo systemctl restart hive-server2
+
+echo "successfully completed the setup for hive to bigquery migration run the next file start.sh "
+
+cd Hive-to-bigquery
 createinsert.sql
