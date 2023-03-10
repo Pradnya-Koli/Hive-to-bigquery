bf793d734e495eb7db5eb2eca5236e501d41b5d8
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-03-10 12:50:56 +0530

    Update Initial-auto.sh

--- Initial-auto.sh
+++ Initial-auto.sh
@@ -13,29 +13,3 @@ gcloud dataproc clusters create demo-hive-to-b2 \
 --service-account=test-demo@searce-practice-data-analytics.iam.gserviceaccount.com
 echo "Connecting To compute ssh "
 gcloud compute ssh demo-hive-to-b2-m --project=searce-practice-data-analytics --zone=us-central1-f
-sudo su
-cd /opt
-
-sudo apt-get update
-sudo apt-get install python3-pip
-wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
-sh /tmp/Anaconda3-2019.10-Linux-x86_64.sh
-source ~/.bashrc
-echo "   activate conda "
-conda activate
-echo "   cloning the git repo "
-git clone https://github.com/Pradnya-Koli/Hive-to-bigquery.git
-
-echo " Installing all the required packages :
-bash prerequisite-installation.sh
-
-echo "  setup the Hive acid transaction configuration file "
-bash auto-conf-setup-incremental.sh
-
-echo "  Restart the hive server "
-sudo systemctl restart hive-server2
-
-echo "successfully completed the setup for hive to bigquery migration run the next file start.sh "
-
-cd Hive-to-bigquery
-createinsert.sql
