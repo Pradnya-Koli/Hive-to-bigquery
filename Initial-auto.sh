0b4d18f5d9f0abfc276d3dbd30eac5dd26c4d8db
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-03-10 10:05:30 +0530

    Create Initial-auto.sh

---
+++ Initial-auto.sh
@@ -0,0 +1,27 @@
+gcloud dataproc clusters create demo-hive-to-b2 \
+--enable-component-gateway \
+--region us-central1  \
+--zone us-central1-f  \
+--master-machine-type n2-standard-4 \
+--master-boot-disk-size 500 \
+--num-workers 2 \
+--worker-machine-type n2-standard-4 \
+--worker-boot-disk-size 500 \
+--image-version 2.0-debian10 \
+--project searce-practice-data-analytics \
+--service-account=test-demo@searce-practice-data-analytics.iam.gserviceaccount.com
+
+gcloud compute ssh demo-hive-to-b2-m --project=searce-practice-data-analytics --zone=us-central1-f
+sudo su
+cd /opt
+
+sudo apt-get update
+sudo apt-get install python3-pip
+wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+sh /tmp/Anaconda3-2019.10-Linux-x86_64.sh
+source ~/.bashrc
+
+git clone https://github.com/Pradnya-Koli/Hive-to-bigquery.git
+bash prerequisite-installation.sh
+bash auto-conf-setup-incremental.sh
+createinsert.sql
